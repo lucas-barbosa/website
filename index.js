2104e6cda490e1f2eba48f3d0d0880fa663237f5
salah alhashmi <alhashmis246@gmail.com>
2021-08-13 06:14:59 -0700

    add env examples

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import morgan from 'morgan';
 
+require('dotenv').config()
+
 const app = express();
 const PORT = process.env.PORT || 8080;
+const test = process.env.DATABASE_URI
 
 // middleware
 
