c82850707d1e91d200c211fc6255a438f4f590bb
salah alhashmi <alhashmis246@gmail.com>
2021-09-17 11:19:46 -0700

    add

--- controllers/subscribers.js
+++ controllers/subscribers.js
@@ -1,4 +1,23 @@
 const subscribersModel = require("../models/subscribersModel");
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    secure:true,
+    service: 'gmail',
+    auth:{
+        user:process.env.EMAIL,
+        pass:process.env.PASS
+    }
+})
+
+const sendEmailToNewSubscriber = (email) => {
+    transporter.sendMail({
+        from:"alguerocode full-stack developer",
+        to:email,
+        subject:"thank you for subscribing",
+        text:"thank you for subscribing to my alguerocode portfolio website , I hope you to see my github account https://github.com/alguerocode"
+    }).catch(err => console.log(err));
+}
 
 const put_new_subscriber = (req, res) =>{
     const {email} = req.body;
@@ -7,6 +26,7 @@ const put_new_subscriber = (req, res) =>{
             res.setHeader("Content-Type", "text/plain");
             res.status(500).send(err.message);
         }
+        sendEmailToNewSubscriber(email);
         res.status(201).json(subscriber);
     });
 }
